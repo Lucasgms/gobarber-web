616259461a578756cc9a43862902248915e27d35
Lucas Gregory <lgregory12@hotmail.com>
2020-08-19 20:18:16 -0300

    feat: profile
    profile page initial structure

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,131 @@
+import React, { useCallback, useRef } from 'react';
+import { FiUser, FiMail, FiLock, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { useHistory, Link } from 'react-router-dom';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
+
+import { Container, Content, AvatarInput, Header } from './styles';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { useAuth } from '../../hooks/auth';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const { user } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('users', data);
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode fazer seu logon no GoBarber!',
+        });
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer cadastro, tente novamente',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Header>
+        <Link to="/dashboard">
+          <FiArrowLeft />
+        </Link>
+      </Header>
+      <Content>
+        <Form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <button type="button">
+              <FiCamera />
+            </button>
+          </AvatarInput>
+
+          <h1>Meu perfil</h1>
+
+          <Input name="name" type="text" placeholder="Nome" icon={FiUser} />
+          <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            name="old_password"
+            type="password"
+            placeholder="Senha atual"
+            icon={FiLock}
+          />
+
+          <Input
+            name="password"
+            type="password"
+            placeholder="Nova Senha"
+            icon={FiLock}
+          />
+
+          <Input
+            name="password_confirmation"
+            type="password"
+            placeholder="Confirmar Senha"
+            icon={FiLock}
+          />
+
+          <Button type="submit">Confirmar mudanças</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
